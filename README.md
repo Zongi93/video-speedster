36f70e8ba866d20731193048031664175a58e8c3
Zongi93 <dearon@indamail.hu>
2019-01-29 22:02:35 +0100

    First release

---
+++ README.md
@@ -0,0 +1,11 @@
+HTML5 video playback speedster
+------------------------------
+
+### How to install
+This is a small chrome plugin, simply enable developer mode and then select the downloaded folder.
+
+### How to use
+Whenever a valid HTML element is present on the page, the plugins icon will light up, then upon clicking it will speed up all playback to 2.5 times the normal speed, but you can decrease/increase this with the numerical input (mouse hover->scroll also works).
+
+### Disclaimer
+This is just something I done in a few minutes, its not feature complete, nor will it probably ever be.
---
+++ popup.js
@@ -0,0 +1,46 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+"use strict";
+
+const playbackSpeed = document.getElementById("playbackSpeed");
+query();
+
+playbackSpeed.onchange = () => query();
+window.onmousewheel = event => {
+  // workaround till faulty mouse is replaced
+  if (!event.altKey) {
+    increaseSpeed();
+  } else {
+    decreaseSpeed();
+  }
+};
+
+function query() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.executeScript(tabs[0].id, {
+      code: `vid = [...document.getElementsByTagName("video")].forEach(video => (video.playbackRate = ${
+        playbackSpeed.value
+      }));`
+    });
+  });
+}
+
+// round to one decimal
+function round(toRound) {
+  return Math.round(toRound * 10) / 10;
+}
+
+function increaseSpeed() {
+  playbackSpeed.value = round(Number(playbackSpeed.value) + 0.1);
+  query();
+}
+
+function decreaseSpeed() {
+  if (playbackSpeed.value > 1)
+    playbackSpeed.value = round(Number(playbackSpeed.value) - 0.1);
+  else playbackSpeed.value = 1;
+
+  query();
+}
